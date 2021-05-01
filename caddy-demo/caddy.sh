242e072a65c93c9034a21712250712535627929c
Bjoern Stuetz <bjoern.stuetz@ybiyr.it>
2021-05-01 10:41:23 +0200

    add caddy demo

---
+++ caddy-demo/caddy.sh
@@ -0,0 +1,29 @@
+VM_NAME=${VM_NAME:-'caddy1'}
+VM_IMAGE=${VM_IMAGE:-'21.14'}
+VM_MEM=${VM_MEM:-'1G'}
+VM_DISK=${VM_DISK:-'1G'}
+
+multipass launch --name "${VM_NAME}" --mem "${VM_MEM}" --disk "${VM_DISK}" "${VM_IMAGE}"
+
+multipass mount -u "$(id -u)":1001 -g "$(id -g)":1001 "$(pwd)" "${VM_NAME}":/home/ubuntu/files
+
+multipass exec "${VM_NAME}" -- bash -c 'sudo apt-get update && sudo apt-get upgrade -y'
+
+multipass exec "${VM_NAME}" -- bash -c 'sudo apt-get install -y debian-keyring debian-archive-keyring apt-transport-https'
+multipass exec "${VM_NAME}" -- bash -c 'curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo apt-key add -'
+multipass exec "${VM_NAME}" -- bash -c 'curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee -a /etc/apt/sources.list.d/caddy-stable.list'
+multipass exec "${VM_NAME}" -- bash -c 'sudo apt-get update'
+multipass exec "${VM_NAME}" -- bash -c 'sudo apt-get install -y caddy'
+
+multipass exec "${VM_NAME}" -- bash -c 'sudo systemctl enable --now caddy.service'
+
+curl $(multipass list | grep "${VM_NAME}" | head -n 1 | awk '{print $3}')
+
+mkdir -p $(pwd)/var/www/html
+
+multipass exec "${VM_NAME}" -- bash -c 'sudo mkdir -p /var/www/html'
+multipass exec "${VM_NAME}" -- bash -c 'sudo sed -i 's#/usr/share/caddy#/var/www/html#g' /etc/caddy/Caddyfile'
+
+multipass mount -u "$(id -u)":1 -g "$(id -g)":1 "$(pwd)/var/www/html" "${VM_NAME}":/var/www/html
+
+multipass exec "${VM_NAME}" -- bash -c 'sudo systemctl restart caddy.service'
